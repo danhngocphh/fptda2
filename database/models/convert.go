806191c6bb9b176f30208b7cc749675ce7584854
vegarnom <danhngocphh@gmail.com>
2021-02-26 08:34:15 +0700

    update

--- database/models/convert.go
+++ database/models/convert.go
@@ -7,42 +7,18 @@ import (
 
 // Post data model
 type Convert struct {
-	Voice   string `sql:"type:text;"`
+	Voice  string `sql:"type:text;"`
 	Text   string `sql:"type:text;"`
-	Speed   int `sql:"type:int;"`
-	Format   string `sql:"type:text;"`
+	Speed  int    `sql:"type:int;"`
+	Format string `sql:"type:text;"`
 }
-
-c.ghhh()
-
-
-PlayerInterface.java
-class interface PlayerInterface () {
-	static function next();
-}
-PlayerController.java
-class PlayerController implement PlayerInterface () {
-	static function next(){
-		int a = 1;
-		return a;
-	};
-}
-
-main.java
-public static function main([]){
-	PlayerInterface model = new PlayerController();
-}
-
-
-
-
 
 // Serialize serializes post data
 func (c Convert) Serialize() common.JSON {
 	return common.JSON{
-		"Voice":       c.Voice,
-		"Text":       c.Text,
-		"Speed":       c.Speed,
-		"Format":       c.Format,
+		"Voice":  c.Voice,
+		"Text":   c.Text,
+		"Speed":  c.Speed,
+		"Format": c.Format,
 	}
 }
